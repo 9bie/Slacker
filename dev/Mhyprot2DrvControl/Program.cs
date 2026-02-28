38035223580662bdceba9e89e4ad325e8495079d
9bie <blackguwc@163.com>
2026-02-28 17:48:33 +0800

    add Mhyprot2DrvControl x2

---
+++ dev/Mhyprot2DrvControl/Program.cs
@@ -0,0 +1,39 @@
+﻿using MhyProt2Drv.Driver;
+using MhyProt2Drv.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MhyProt2Drv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DrvLoader loader = new DrvLoader();
+            loader.Load();
+            MhyProt2 mhyprot = new MhyProt2();
+            mhyprot.OpenDrv();
+            
+            bool res = mhyprot.InitDrv((ulong)Process.GetCurrentProcess().Id);
+            if (!res)
+            {
+                Console.WriteLine("Init Error!");
+            }
+            else
+            {
+
+                mhyprot.KillProcess((uint)int.Parse(args[0]));
+                Console.WriteLine($"[+] Kill Process Successful:{args[0]:X}");
+            }
+
+            mhyprot.CloseHandle();
+            loader.UnLoad();
+        }
+    }
+}
---
+++ dev/Mhyprot2DrvControl/README.md
@@ -0,0 +1,9 @@
+# Mhyprot2DrvControl
+A lib that allows using mhyprot2 driver for enum process modules, r/w process memory and kill process.
+
+# Usage
+Build and run, see Driver\MhyProt2.cs for available function and usage.
+
+# Demo
+![Demo](https://raw.githubusercontent.com/kagurazakasanae/Mhyprot2DrvControl/main/demo.png)
+![KillProcessDemo](https://raw.githubusercontent.com/kagurazakasanae/Mhyprot2DrvControl/main/killprocessdemo.gif)
